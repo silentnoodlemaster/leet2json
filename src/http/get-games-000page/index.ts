463644019945381e5fd6831c87415b334797d35e
silentnoodlemaster <silentnoodle@cock.li>
2021-10-18 16:01:09 +0300

    move code

--- src/http/get-games-000page/index.ts
+++ src/http/get-games-000page/index.ts
@@ -1,4 +1,43 @@
-import Leet from '../../scrapers/list.ts';
+import { DOMParser } from 'https://deno.land/x/deno_dom@v0.1.15-alpha/deno-dom-wasm.ts';
+import { HTMLElement } from 'https://github.com/microsoft/TypeScript/blob/main/lib/lib.dom.d.ts'
+
+class Leet {
+  root: string;
+  base: string;
+  items: Record<string, string|null>[] = [];
+  pages = 0;
+
+  constructor(public name: string) {
+    this.root = "https://1337x.to/";
+    this.base = `${this.root}${name}-torrents/`;
+  }
+  async build(page = 1) {
+    console.log(`${this.base}${page}/`);
+    const res = await fetch(`${this.base}${page}/`);
+    const html = await res.text();
+    const document = new DOMParser().parseFromString(html, 'text/html');
+    if (document) {
+      this.pages = parseInt(document.querySelector('li.last a')!.getAttribute('href')!.split('/')[2]);
+      document.querySelectorAll('td.coll-1.name').forEach((e: HTMLElement) => {
+        e.querySelectorAll('a:not(.icon)').forEach((a: HTMLElement) => {
+          const href = a.getAttribute('href');
+          const name = a.textContent;
+          this.items.push({
+            name: name,
+            url: href
+          });
+        });
+      });
+    }
+    const body = {items: this.items, pages: this.pages} as any;
+    if (page+1 < this.pages )
+      body.nextPage = page+1;
+    if (page-1 > 1)
+      body.prevPage = page -1;
+    
+    return body;
+  }
+}
 
 export async function handler (req: any) {
   const leet = new Leet('johncena141');
